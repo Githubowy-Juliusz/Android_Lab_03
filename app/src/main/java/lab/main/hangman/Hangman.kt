85bdd90fb091ae8e0607f3aadcc7f81e337d829b
githubowy-juliusz <githubowy-juliusz@wp.pl>
2020-11-09 13:06:37 +0100

    Hangman fully implemented.

--- app/src/main/java/lab/main/hangman/Hangman.kt
+++ app/src/main/java/lab/main/hangman/Hangman.kt
@@ -3,21 +3,27 @@ package lab.main.hangman
 import java.util.*
 
 class Hangman(private val wordToGuess: String) {
-	private val MAX_TRIES = 6
+	private val MAX_MISTAKES = 6
 	private val _usedLetters = mutableListOf<Char>()
+	private var mistakes = 0
 
 	var currentTry = 0
 		private set
+
 	val usedLetters: List<Char>
 		get() {
 			return Collections.unmodifiableList(_usedLetters)
 		}
-	val remainingTries: Int
+
+	val possibleMistakesRemaining: Int
 		get() {
-			return MAX_TRIES - currentTry
+			return MAX_MISTAKES - mistakes
 		}
+
 	val encryptedWordToGuess: String
 		get() {
+			if(possibleMistakesRemaining == 0)
+				return wordToGuess
 			val tmpList = mutableListOf<Char>()
 			for(c in wordToGuess) {
 				if(c in _usedLetters)
@@ -31,9 +37,27 @@ class Hangman(private val wordToGuess: String) {
 	fun guess(letter: Char): Boolean {
 		if(letter in _usedLetters)
 			return false
+		if(possibleMistakesRemaining == 0)
+			return false
+
 		currentTry++
+		_usedLetters.add(letter)
+		countAndSetMistakes()
+
+		for(c in wordToGuess) {
+			if(c !in _usedLetters)
+				return false
+		}
+		mistakes = MAX_MISTAKES
 		return true
 	}
 
+	private fun countAndSetMistakes() {
+		var tmpMistakes = 0
+		for(c in _usedLetters)
+			if(c !in wordToGuess)
+				tmpMistakes++
+		mistakes = tmpMistakes
+	}
 
 }
--- app/src/main/java/lab/main/hangman/HangmanFragment.kt
+++ app/src/main/java/lab/main/hangman/HangmanFragment.kt
@@ -23,5 +23,46 @@ class HangmanFragment :
 			view.findViewById<TextView>(R.id.hangmanRemainingTriesTextView)
 		val letterInput = view.findViewById<EditText>(R.id.hangmanLetterEditText)
 		val confirmButton = view.findViewById<Button>(R.id.hangmanConfirmButton)
+
+		val dictionary =
+			listOf("apple", "noun", "ice", "pear", "pineapple", "hammer", "water")
+
+		var hangman = Hangman(dictionary.random())
+
+		fun updateFields() {
+			letterInput.text.clear()
+			tryNumberText.text = "Tries: " + hangman.currentTry.toString()
+			wordToGuessText.text = "Word to guess: " + hangman.encryptedWordToGuess
+			usedLettersText.text =
+				"Used letters: " + hangman.usedLetters.joinToString(separator = ", ")
+			remainingTriesText.text =
+				"Remaining mistakes: " + hangman.possibleMistakesRemaining.toString()
+		}
+
+		updateFields()
+
+		confirmButton.setOnClickListener() {
+			if(hangman.possibleMistakesRemaining == 0) {
+				hangman = Hangman(dictionary.random())
+				confirmButton.text = "Confirm"
+				updateFields()
+				return@setOnClickListener
+			}
+			val character: Char
+			try {
+				character = letterInput.text[0]
+			} catch(e: IndexOutOfBoundsException) {
+				return@setOnClickListener
+			}
+			val win = hangman.guess(character)
+			updateFields()
+			if(win) {
+				confirmButton.text = "Try again"
+				tryNumberText.text = "You won!"
+			} else if(hangman.possibleMistakesRemaining == 0) {
+				tryNumberText.text = "You lost."
+				confirmButton.text = "Try again"
+			}
+		}
 	}
 }
